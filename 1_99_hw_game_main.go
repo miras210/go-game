957409f0bc8fe0022bf11ba4e3d94f0f389376c0
Miras <sektor.miras@gmail.com>
2020-11-13 21:53:05 +0600

    added: basic game structure, empty command handler

--- 1_99_hw_game_main.go
+++ 1_99_hw_game_main.go
@@ -1,9 +1,83 @@
 package main
 
-/*
-	код писать в этом файле
-	наверняка у вас будут какие-то структуры с методами, глобальные перменные ( тут можно ), функции
-*/
+import (
+	"strings"
+)
+
+type Location struct {
+	name      string
+	neighbors []Location
+	places    []Place
+}
+
+func (l *Location) addNeighbour(n Location) {
+	l.neighbors = append(l.neighbors, n)
+}
+
+func (l *Location) getNeighbours() string {
+	length := len(l.neighbors)
+	ans := "можно пройти - "
+	for i := 0; i < length; {
+		ans += l.neighbors[i].name
+		if i != length-1 {
+			ans += ", "
+		}
+	}
+	return ans
+}
+
+func (l *Location) addPlace(p Place) {
+	l.places = append(l.places, p)
+}
+
+func (l *Location) getPlaces() string {
+	ans := ""
+	length := len(l.places)
+	for i := 0; i < length; {
+		ans += l.places[i].getObjects()
+		if i != length-1 {
+			ans += ", "
+		}
+	}
+	return ans
+}
+
+type Place struct {
+	name    string
+	objects []Object
+}
+
+func (p *Place) getObjects() string {
+	length := len(p.objects)
+	ans := "на " + p.name + ": "
+	for i := 0; i < length; {
+		ans += p.objects[i].getName()
+		if i != length-1 {
+			ans += ", "
+		}
+	}
+	return ans
+}
+
+type Object interface {
+	getName() string
+}
+
+type Equipment struct {
+	name string
+}
+
+func (e *Equipment) getName() string {
+	return e.name
+}
+
+type Item struct {
+	name string
+}
+
+func (i *Item) getName() string {
+	return i.name
+}
 
 func main() {
 	/*
@@ -18,12 +92,23 @@ func initGame() {
 		эта функция инициализирует игровой мир - все команты
 		если что-то было - оно корректно перезатирается
 	*/
+
 }
 
 func handleCommand(command string) string {
-	/*
-		данная функция принимает команду от "пользователя"
-		и наверняка вызывает какой-то другой метод или функцию у "мира" - списка комнат
-	*/
-	return "not implemented"
+	s := strings.Split(command, " ")
+	switch s[0] {
+	case "осмотреться":
+		return "осматриваюсь"
+	case "идти":
+		return "иду"
+	case "надеть":
+		return "надеваю"
+	case "взять":
+		return "беру"
+	case "применить":
+		return "применяю"
+	default:
+		return "неизвестная команда"
+	}
 }
